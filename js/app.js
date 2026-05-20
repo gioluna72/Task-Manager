ca92a19c01e35f0d25beb7e39b9acf84d8ca928f
gioluna72 <baxter12722@gmail.com>
2026-05-20 01:24:34 -0600

    Mi primer commit

--- js/app.js
+++ js/app.js
@@ -2,14 +2,9 @@ const input = document.querySelector("input"); //Selecciona el input
 const button = document.querySelector("button"); //Selecciona el boton
 const tasks = document.querySelector("ul"); //Selecciona la lista
 
-button.addEventListener("click", () => { //Se activa al dar click al boton
-    const newItem = document.createElement("li"); //Se crea un nuevo elemento
-    newItem.textContent = input.value; //Se asigna el valor del input al nuevo elemento
-    tasks.appendChild(newItem); //Se agrega el nuevo elemento a la lista
-    input.value = ""; //Se limpia el input
-});
+button.addEventListener("click", addTask); //Se activa al dar click al boton
 
-function newTask() { //Se define una funcion
+function addTask() { //Se define una funcion
     if (input.value.trim() === "") return; // Se asegura de que el input no esté vacío
     const newItem = document.createElement("li"); //Se crea un nuevo elemento
     newItem.textContent = input.value; //Se asigna el valor del input al nuevo elemento
