73bef096ba98a7ac7e8bf93becb5c0005bb559fe
gioluna72 <baxter12722@gmail.com>
2026-05-23 22:07:33 -0600

    cuarto cambio

--- js/app.js
+++ js/app.js
@@ -12,26 +12,50 @@ if (savedTasks) {
 
 button.addEventListener("click", addTask);
 
-function addTask() {    
+async function addTask() {
     const text = input.value;
-    if(text === "") return;
-    tasks.push(text);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    renderTasks();
+    if (text === "") return;
+
+    await supabase
+        .from("tasks")
+        .insert({ text })
+
+    getTasks();
     input.value = "";
 }
 
-function renderTasks() {
-    list.innerHTML = ""; 
+async function getTasks() {
+    const { data, error } = await supabase
+        .from("Tasks")
+        .select("*");
 
-    tasks.forEach(function(task) {
+    if (error) {
+        console.log("Error al obtener las tareas:", error);
+        return;
+    }
+
+    tasks = data;
+    renderTasks();
+}
+
+function renderTasks() {
+    list.innerHTML = "";
+
+    tasks.forEach(function (task) {
         const listItem = document.createElement("li");
-        listItem.textContent = task;
-        list.appendChild(listItem); 
+        listItem.textContent = task.text;
+        list.appendChild(listItem);
     });
 }
 
 function deleteTask() {
     tasks.pop();
     renderTasks();
-}   
+}
+
+getTasks();
+
+const supabaseUrl = "https://bfuqqdgngkvlajrepyjl.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmdXFxZGduZ2t2bGFqcmVweWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MTcxNDcsImV4cCI6MjA5NDk5MzE0N30.B1zlQAIdq0FhzEq0cNrRhP1BlmQbdcEN5U3Mi1OCUpw";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+console.log(supabase);   
