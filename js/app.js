463747c895ec3d7669450bb56318879d4f3003ef
gioluna72 <baxter12722@gmail.com>
2026-05-27 23:23:54 -0600

    quinto cambio

--- js/app.js
+++ js/app.js
@@ -12,22 +12,41 @@ if (savedTasks) {
 
 button.addEventListener("click", addTask);
 
+
 async function addTask() {
+
+    const user = await getUser();
+
+    if (!user) {
+        console.log("Usuario no logueado");
+        return;
+    }
+
     const text = input.value;
+
     if (text === "") return;
 
-    await supabase
-        .from("tasks")
-        .insert({ text })
+    await supabaseClient
+        .from("Tasks")
+        .insert({ text, user_id: user.id })
 
     getTasks();
     input.value = "";
 }
 
 async function getTasks() {
-    const { data, error } = await supabase
+
+    const user = await getUser();
+
+    if (!user) {
+        console.log("Usuario no logueado");
+        return;
+    }
+
+    const { data, error } = await supabaseClient
         .from("Tasks")
-        .select("*");
+        .select("*")
+        .eq("user_id", user.id);
 
     if (error) {
         console.log("Error al obtener las tareas:", error);
@@ -54,6 +73,51 @@ function deleteTask() {
 }
 
 getTasks();
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("login");
+const registerBtn = document.getElementById("register");
+
+registerBtn.addEventListener("click", async () => {
+    const { data, error } = await supabase
+        .auth
+        .signUp({
+            email: email.value,
+            password: password.value,
+        });
+
+    if (error) {
+        console.log("Error al registrar:", error);
+        return;
+    }
+
+    console.log("Usuario registrado:", data.user);
+});
+
+loginBtn.addEventListener("click", async () => {
+    const { data, error } = await supabase
+        .auth
+        .signInWithPassword({
+            email: email.value,
+            password: password.value,
+        });
+
+    if (error) {
+        console.log("Error al iniciar sesión:", error);
+        return;
+    }
+
+    console.log("Usuario logueado:", data.user);
+});
+
+async function getUser() {
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (user) {
+        console.log("Usuario:", user);
+    }
+}
 
 const supabaseUrl = "https://bfuqqdgngkvlajrepyjl.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmdXFxZGduZ2t2bGFqcmVweWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MTcxNDcsImV4cCI6MjA5NDk5MzE0N30.B1zlQAIdq0FhzEq0cNrRhP1BlmQbdcEN5U3Mi1OCUpw";
